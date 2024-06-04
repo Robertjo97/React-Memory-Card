7729616470ba028dd2eee9a936b9c550a9a5e49b
Robertjo97 <robertjo97@gmail.com>
2024-06-04 12:14:18 -0700

    modify title

---
+++ src/components/Title.jsx
@@ -0,0 +1,7 @@
+function Title() {
+    return (
+        <h1 className="title">Pokemon Memory Game</h1>
+    );
+}
+
+export default Title
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Title from './components/Title.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Title />
   </React.StrictMode>,
 )
