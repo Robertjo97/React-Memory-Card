eb84017051fbc9e64e8b2395bd100b85bb715c41
Robertjo97 <robertjo97@gmail.com>
2024-06-09 17:16:44 -0700

    create card.jsx

---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+const Card = ({name, sprite}) => {
+    return (
+        <div className="card">
+            <h3>{name}</h3>
+            <img src={sprite} alt={name}></img>
+        </div>
+    )
+}
